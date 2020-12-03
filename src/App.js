99e9070c3c45bdbead767f061c7343af644cb27a
victorreis <v.crisostomo.reis@gmail.com>
2020-12-03 14:39:49 -0300

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Link, Route, BrowserRouter, Switch } from 'react-router-dom';
 
 import './App.css';
+import About from './Pages/About';
+import Home from './Pages/Home';
+import User from './Pages/User';
 
 const App = () => {
     return (
         <>
-            <Button>Teste</Button>
+            <BrowserRouter>
+                <div>
+                    <nav>
+                        <Link to="/">
+                            <Button>Home</Button>
+                        </Link>
+                        <Link to="/about">
+                            <Button>About</Button>
+                        </Link>
+                    </nav>
+
+                    <Switch>
+                        <Route path="/about" exact component={About} />
+                        <Route path="/" exact component={Home} />
+                        <Route path="*" exact component={User} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
         </>
     );
 };
