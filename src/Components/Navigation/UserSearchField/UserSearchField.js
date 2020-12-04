a12eacb80c1e4b556bf3a45a497050f25e72009a
victorreis <v.crisostomo.reis@gmail.com>
2020-12-03 21:41:11 -0300

    Implement search by username functionality

--- src/Components/Navigation/UserSearchField/UserSearchField.js
+++ src/Components/Navigation/UserSearchField/UserSearchField.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
-const UserSearchField = (props) => (
-    <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-success">Search</Button>
-    </Form>
-);
+import { validateGithubUsername } from '../../../Utils/validators';
+
+const UserSearchField = (props) => {
+    const history = useHistory();
+    const [username, setUsername] = useState('');
+    const [isValidUsername, setIsValidUsername] = useState(true);
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+        if (!e.target.value || validateGithubUsername(e.target.value)) {
+            setIsValidUsername(true);
+        } else {
+            setIsValidUsername(false);
+        }
+    };
+    const classes = ['mr-sm-2', isValidUsername ? '' : 'is-invalid'];
+
+    const handleSearch = () => {
+        // eslint-disable-next-line fp/no-mutating-methods
+        history.push(username);
+    };
+
+    return (
+        <Form inline>
+            <FormControl
+                type="text"
+                placeholder="Search"
+                className={classes.join(' ')}
+                value={username}
+                onChange={handleUsernameChange}
+            />
+            <Button
+                variant="outline-success"
+                onClick={handleSearch}
+                disabled={!isValidUsername}>
+                Search
+            </Button>
+        </Form>
+    );
+};
 
 export default UserSearchField;
