cf3987e2d058da14583c1eab78f3ee98318fcf7f
victorreis <v.crisostomo.reis@gmail.com>
2020-12-05 19:59:43 -0300

    Add initial tests with mocha

---
+++ tests/Services/GithubUserService.spec.js
@@ -0,0 +1,11 @@
+describe('Services/GithubUserService', () => {
+    describe('getByUsername', () => {
+        context('Successful request - status 200', () => {
+            it('Should return the user informations', () => {});
+        });
+
+        context('Unsuccessful request - status 404', () => {
+            it('Should return an empty object', () => {});
+        });
+    });
+});
